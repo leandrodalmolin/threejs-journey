83d5b2566e440ab6b81e0e9a45fd17b601cbad27
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2025-01-31 15:07:58 -0300

    C1 L8 Final

--- chapter-01/08-geometries/src/script.js
+++ chapter-01/08-geometries/src/script.js
@@ -11,8 +11,64 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
+
+/**
+ * Custom geometry (simple triangle)
+ */
+// const positionsArray = new Float32Array(9);
+
+// // Vertex 1
+// positionsArray[0] = 0;
+// positionsArray[1] = 0;
+// positionsArray[2] = 0;
+
+// // Vertex 2
+// positionsArray[3] = 0;
+// positionsArray[4] = 1;
+// positionsArray[5] = 0;
+
+// // Vertex 3
+// positionsArray[6] = 1;
+// positionsArray[7] = 0;
+// positionsArray[8] = 0;
+
+// // Alternative way
+// // const positionsArray = new Float32Array([
+// //   0, 0, 0, // Vertex 1
+// //   0, 1, 0, // Vertex 2
+// //   1, 0, 0, // Vertex 3
+// // ]);
+
+// const positionsAttribute = new THREE.BufferAttribute(
+//   positionsArray,
+//   3 // specifying we have 3 values per vertex (we would have 2 for a UV value)
+// );
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute("position", positionsAttribute);
+
+/**
+ * Custom geometry (bunch of random triangles)
+ */
+const geometry = new THREE.BufferGeometry();
+
+const count = 50;
+const positionsArray = new Float32Array(count * 3 * 3); // 50 * 3 vertices * 3 values per vertex
+
+for (let i = 0; i < positionsArray.length; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4; // -4 to 4
+}
+
+const positionsAttribute = new THREE.BufferAttribute(
+  positionsArray,
+  3 // specifying we have 3 values per vertex (we would have 2 for a UV value)
+);
+geometry.setAttribute("position", positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
