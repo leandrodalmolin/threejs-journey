f212609c0b1e5edf34705492ebe9e46d403fad4d
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2025-04-27 09:52:31 +0100

    C2L17 Final

--- chapter-02/17-particles/src/script.js
+++ chapter-02/17-particles/src/script.js
@@ -18,15 +18,79 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 20000;
+
+// Float array is a performant way to handle positions
+// You have a sequential array where indexes 0, 1, 2 will be one position (x, y, z values)
+// 3, 4, 5 another position, 6, 7, 8 another. If we want to have 500 points,
+// then we need to multiply it by 3 to have slots for x, y, z of each position
+const positions = new Float32Array(count * 3); // x, y, z
+const colors = new Float32Array(count * 3); // r, g, b (needs to activate "particlesMaterial.vertexColors" to work)
+
+// Fill the array with position values
+for (let i = 0; i < positions.length; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
 );
-scene.add(cube);
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial();
+particlesMaterial.size = 0.1;
+particlesMaterial.sizeAttenuation = true; // create perspective, far particles are small and the close ones big
+// particlesMaterial.color = new THREE.Color("#ff88cc");
+particlesMaterial.vertexColors = true; // this is to make colors being added via array above to work
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// When we apply the texture to a particle we have issues with transparency
+// Due to the order they are created, the GPU has a hard time identifying which
+// ones are in front of each other, so you can see edges, not fully transparent
+
+// First solution would be set alphaTest, telling from each point the alpha should be rendered
+// It is not perfect but could be a good solution for some situations
+// particlesMaterial.alphaTest = 0.001;
+
+// Second solution, deactivating alphaTest.
+// This make the GPU stop to try to guess if the elements are in front of each other.
+// It works but we shouldn't do it. We gonna have bugs if we have elements with different colours
+// E.g: when adding a white cube among the particles you gonna be able to see particles in the middle
+// of the cube, when they shouldn't be visible since they are behind it.
+// particlesMaterial.depthTest = false;
+
+// Third solution:
+// The WebGL is testing if what's being drawn is closer than what's already drawn.
+// The depth of what's being drawn is stored in what we call a depth buffer.
+// Instead of not testing if the particle is closer than what's in this depth buffer,
+// we can tell the WebGL not to write particles in that depth buffer.
+// It might lead to some bugs sometimes but in general a good solution.
+particlesMaterial.depthWrite = false;
+
+// Makes colors blend. With the transparency and blend, when particles are
+// on top of each other, they make colors brighter
+particlesMaterial.blending = THREE.AdditiveBlending;
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+// Cube
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(),
+//   new THREE.MeshBasicMaterial()
+// );
+// scene.add(cube);
 
 /**
  * Sizes
@@ -83,6 +147,27 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update group of particles
+  // particles.rotation.y = elapsedTime * 0.2;
+
+  // IMPORTANT: This is an example on how we could animate the positions
+  // and it is alright to do it if we had like 15 particles.
+  // We shouldn't do the wave effect this way if we have a lot of particles,
+  // we should use custom shaders instead (will learn it in the future)
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    const xIndex = i3 + 0;
+    const yIndex = i3 + 1;
+    // We need to create an offset to create an wave effect, so we add the x position to the elapsedTime
+    const xValue = particlesGeometry.attributes.position.array[xIndex];
+    particlesGeometry.attributes.position.array[yIndex] = Math.sin(
+      elapsedTime + xValue
+    );
+    // we need the below otherwise particles won't animate.
+    // we need to inform three.js they are changing
+    particlesGeometry.attributes.position.needsUpdate = true;
+  }
 
   // Update controls
   controls.update();
